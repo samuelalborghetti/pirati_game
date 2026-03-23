a430de3b48359da06f89800e436b06918b3702d4
samuel alborghetti <alborghetti.samuel.09itisdalmine.edu.it>
2026-03-23 10:11:40 +0100

    cambio struttura dati

--- gioco.py
+++ gioco.py
@@ -1,78 +1,81 @@
 import pygame
+import json
 
 pygame.init()
 
 LARGHEZZA = 1200
 ALTEZZA = 800
-NERO = (155, 0, 0)
+NERO = (255, 0, 0)
 
 personaggi = [
     {
-        "name": "Capitano",
-        "cost": 380,
-        "hp": 3,
-        "alive": True,
+        "stats": {
+            "cost": 380,
+            "hp": 3,
+            "alive": True
+        },
         "sprites": {
             "idle":         [pygame.image.load(f"allframe/capitano/idle/capitanoidle{i}.png") for i in range(1, 3)],
             "walk_forward": [pygame.image.load(f"allframe/capitano/camminata_in_avanti/capitano{i}_camminatainavanti.png") for i in range(1, 5)],
             "walk_cycle":   [pygame.image.load(f"allframe/capitano/camminata_a_destrasinistra_con_flip/camminata_laterale{i}.png") for i in range(1, 5)]
         },
-        "info" : {
-            "nome" : " ",
-            "descrizione" :"",
-            "abilita" : " "
+        "info": {
+            "nome":        "Capitano",
+            "descrizione": "",
+            "abilita":     ""
         }
     },
     {
-        "name": "Cuoco",
-        "cost": 380,
-        "hp": 3,
-        "alive": True,
+        "stats": {
+            "cost": 380,
+            "hp": 3,
+            "alive": True
+        },
         "sprites": {
             "idle":         [pygame.image.load(f"allframe/cuoco/idle/cuocoidle{i}.png") for i in range(1, 7)],
             "walk_forward": [pygame.image.load(f"allframe/cuoco/camminata_in_avanti/cuoco{i}_camminatainavanti.png") for i in range(1, 3)],
             "walk_cycle":   [pygame.image.load(f"allframe/cuoco/camminata_a_destrasinistra_con_flip/camminata_laterale{i}cuoco.png") for i in range(1, 7)]
         },
-        "info" : {
-            "nome" : " ",
-            "descrizione" :"",
-            "abilita" : " "
+        "info": {
+            "nome":        "Cuoco",
+            "descrizione": "",
+            "abilita":     ""
         }
-        
     },
     {
-        "name": "guardone",
-        "cost": 380,
-        "hp": 3,
-        "alive": True,
+        "stats": {
+            "cost": 380,
+            "hp": 3,
+            "alive": True
+        },
         "sprites": {
             "idle":         [pygame.image.load(f"allframe/guardone/idle/guardoneidle{i}.png") for i in range(1, 9)],
             "walk_forward": [pygame.image.load(f"allframe/guardone/camminata_in_avanti/guardone{i}_camminatainavanti.png") for i in range(1, 5)],
             "walk_cycle":   [pygame.image.load(f"allframe/guardone/camminata_a_destrasinistra_con_flip/camminata_lateraleguardone{i}.png") for i in range(1, 8)]
         },
-        "info" : {
-            "nome" : " ",
-            "descrizione" :"",
-            "abilita" : " "
+        "info": {
+            "nome":        "Guardone",
+            "descrizione": "",
+            "abilita":     ""
         }
     },
     {
-        "name": "medico",
-        "cost": 380,
-        "hp": 3,
-        "alive": True,
-        "sprites": {
-            "idle":         [pygame.image.load(f"allframe/medico/idle/medicoidle{i}.png") for i in range (1,9)],
-            "walk_forward": [pygame.image.load(f"allframe/medico/camminata_in_avanti/medico{i}_camminatainavanti.png") for i in range(1,9)],
-            "walk_cycle":   [pygame.image.load(f"allframe/medico/camminata_a_destrasinistra_con_flip/camminata_lateralecuoco{i}.png") for i in range(1,7)]
+        "stats": {
+            "cost": 380,
+            "hp": 3,
+            "alive": True
         },
-        "info" : {
-            "nome" : " ",
-            "descrizione" :"",
-            "abilita" : " "
+        "sprites": {
+            "idle":         [pygame.image.load(f"allframe/medico/idle/medicoidle{i}.png") for i in range(1, 9)],
+            "walk_forward": [pygame.image.load(f"allframe/medico/camminata_in_avanti/medico{i}_camminatainavanti.png") for i in range(1, 9)],
+            "walk_cycle":   [pygame.image.load(f"allframe/medico/camminata_a_destrasinistra_con_flip/camminata_lateralecuoco{i}.png") for i in range(1, 7)]
+        },
+        "info": {
+            "nome":        "Medico",
+            "descrizione": "",
+            "abilita":     ""
         }
     },
-    
 ]
 
 sfondo = pygame.image.load("generale/sfondo_pirati.jpg")
@@ -102,25 +105,21 @@ while not gameOver:
     schermo.fill(NERO)
 
     disegna_animazione(schermo, personaggi[0]["sprites"], "walk_forward", 150, (200, 100))
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle", 150, (300, 100))
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle", 150, (400, 100), flip=True)
-    disegna_animazione(schermo, personaggi[0]["sprites"], "idle", 200, (500, 100))
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle",   150, (300, 100))
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle",   150, (400, 100), flip=True)
+    disegna_animazione(schermo, personaggi[0]["sprites"], "idle",         200, (500, 100))
     disegna_animazione(schermo, personaggi[1]["sprites"], "walk_forward", 200, (700, 100), (54, 74))
-    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle", 140, (800, 100), (54, 74))
-    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle", 140, (900, 100), (54, 74), flip=True)
-    disegna_animazione(schermo, personaggi[1]["sprites"], "idle", 150, (1000, 100), (54, 74))
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle",   140, (800, 100), (54, 74))
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle",   140, (900, 100), (54, 74), flip=True)
+    disegna_animazione(schermo, personaggi[1]["sprites"], "idle",         150, (1000, 100), (54, 74))
     disegna_animazione(schermo, personaggi[2]["sprites"], "walk_forward", 150, (200, 200), (64, 78))
-    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle", 110, (300, 200), (64, 78), flip=True)
-    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle", 110, (400, 200), (64, 78))
-    disegna_animazione(schermo, personaggi[2]["sprites"], "idle", 150, (500, 200), (64, 78))
-    
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle",   110, (300, 200), (64, 78), flip=True)
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle",   110, (400, 200), (64, 78))
+    disegna_animazione(schermo, personaggi[2]["sprites"], "idle",         150, (500, 200), (64, 78))
     disegna_animazione(schermo, personaggi[3]["sprites"], "walk_forward", 120, (700, 200), (60, 74))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle", 140, (800, 200), (60, 80))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle", 140, (900, 200), (60, 80),  flip=True)
-    disegna_animazione(schermo, personaggi[3]["sprites"], "idle", 135, (995, 200),(66, 80))
-    
-    
-    
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle",   140, (800, 200), (60, 80))
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle",   140, (900, 200), (60, 80), flip=True)
+    disegna_animazione(schermo, personaggi[3]["sprites"], "idle",         135, (995, 200), (66, 80))
 
     pygame.display.update()
     clock.tick(60)
