766a1f40358be6a365252df09ee88c0cff949a72
samuelalborghetti <saalborghetti09@gmail.com>
2026-03-23 17:08:12 +0100

    tutti gli sprite malati

--- gioco.py
+++ gioco.py
@@ -27,7 +27,7 @@ personaggi = [
             "abilita":     ""
         }
     },
-    #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #-------------------------------------------------------------------------------------el maiale 1--------------------------------------------------------------------------------------------
     {
         "stats": {
             "cost": None,
@@ -37,7 +37,8 @@ personaggi = [
         "sprites": {
             "idle":         [pygame.image.load(f"allframe/cuoco/idle/cuocoidle{i}.png") for i in range(1, 7)],
             "walk_forward": [pygame.image.load(f"allframe/cuoco/camminata_in_avanti/cuoco{i}_camminatainavanti.png") for i in range(1, 3)],
-            "walk_cycle":   [pygame.image.load(f"allframe/cuoco/camminata_a_destrasinistra_con_flip/camminata_laterale{i}cuoco.png") for i in range(1, 7)]
+            "walk_cycle":   [pygame.image.load(f"allframe/cuoco/camminata_a_destrasinistra_con_flip/camminata_laterale{i}cuoco.png") for i in range(1, 7)],
+            "walk_cycle_sick":   [pygame.image.load(f"allframe/cuoco/camminata_a_destrasinistra_con_flip_ammalato/camminataavanticuocoammalato{i}.png") for i in range(1, 7)]
         },
         "info": {
             "name":        "Cuoco",
@@ -55,7 +56,8 @@ personaggi = [
         "sprites": {
             "idle":         [pygame.image.load(f"allframe/guardone/idle/guardoneidle{i}.png") for i in range(1, 9)],
             "walk_forward": [pygame.image.load(f"allframe/guardone/camminata_in_avanti/guardone{i}_camminatainavanti.png") for i in range(1, 5)],
-            "walk_cycle":   [pygame.image.load(f"allframe/guardone/camminata_a_destrasinistra_con_flip/camminata_lateraleguardone{i}.png") for i in range(1, 8)]
+            "walk_cycle":   [pygame.image.load(f"allframe/guardone/camminata_a_destrasinistra_con_flip/camminata_lateraleguardone{i}.png") for i in range(1, 8)],
+            "walk_cycle_sick":   [pygame.image.load(f"allframe/guardone/camminata_a_destrasinistra_con_flip_ammalato/camminataavantiguardoneammalato{i}.png") for i in range(1, 8)]
         },
         "info": {
             "name":        "Guardone",
@@ -110,28 +112,37 @@ while not gameOver:
             gameOver = True
 
     schermo.fill(ROSSO)
+    #---------------------------------------------------------------------------capitano----------------------------------------------------------------------------------------------------
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_forward", 150, (50, 50))
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle",   150, (150, 50))
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle",   150, (250, 50), flip=True)
+    disegna_animazione(schermo, personaggi[0]["sprites"], "idle",         200, (350, 50))
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle_sick", 150, (450, 50))
+    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle_sick", 150, (550, 50), flip = True)
 
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_forward", 150, (50, 100))
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle",   150, (150, 100))
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle",   150, (250, 100), flip=True)
-    disegna_animazione(schermo, personaggi[0]["sprites"], "idle",         200, (350, 100))
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle_sick", 150, (450, 100))
-    disegna_animazione(schermo, personaggi[0]["sprites"], "walk_cycle_sick", 150, (550, 100), flip = True)
-    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_forward", 200, (700, 100), (54, 74))
-    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle",   170, (800, 100), (54, 74))
-    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle",   170, (900, 100), (54, 74), flip=True)
-    disegna_animazione(schermo, personaggi[1]["sprites"], "idle",         150, (1000, 100), (54, 74))
-    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_forward", 150, (200, 200), (64, 78))
-    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle",   110, (300, 200), (64, 78), flip=True)
-    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle",   110, (400, 200), (64, 78))
-    disegna_animazione(schermo, personaggi[2]["sprites"], "idle",         150, (500, 200), (64, 78))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_forward", 120, (650, 200), (60, 74))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle",   140, (750, 200), (60, 80))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle",   140, (850, 200), (60, 80), flip=True)
-    disegna_animazione(schermo, personaggi[3]["sprites"], "idle",         135, (945, 197), (66, 76))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle_sick", 140, (1040, 200), (60, 80))
-    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle_sick", 140, (1140, 200), (60, 80), flip = True)
+    #---------------------------------------------------------------------------maile----------------------------------------------------------------------------------------------------
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_forward", 200, (650, 50), (54, 74))
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle",   170, (750, 50), (54, 74))
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle",   170, (850, 50), (54, 74), flip=True)
+    disegna_animazione(schermo, personaggi[1]["sprites"], "idle",         150, (945, 50), (54, 74))
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle_sick",   170, (1040, 50), (54, 74))
+    disegna_animazione(schermo, personaggi[1]["sprites"], "walk_cycle_sick",   170, (1140, 50), (54, 74), flip=True)
 
+    #---------------------------------------------------------------------------guardone----------------------------------------------------------------------------------------------------
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_forward", 150, (50, 150), (64, 78))
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle",   110, (150, 150), (64, 78), flip=True)
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle",   110, (250, 150), (64, 78))
+    disegna_animazione(schermo, personaggi[2]["sprites"], "idle",         150, (350, 150), (64, 78))
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle_sick",   110, (550, 150), (64, 78))
+    disegna_animazione(schermo, personaggi[2]["sprites"], "walk_cycle_sick",   110, (450, 150), (64, 78), flip=True)
+
+    #---------------------------------------------------------------------------medico----------------------------------------------------------------------------------------------------
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_forward", 120, (650, 150), (60, 74))
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle",   140, (750, 150), (60, 80))
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle",   140, (850, 150), (60, 80), flip=True)
+    disegna_animazione(schermo, personaggi[3]["sprites"], "idle",         135, (945, 147), (66, 76))
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle_sick", 140, (1040, 150), (60, 80))
+    disegna_animazione(schermo, personaggi[3]["sprites"], "walk_cycle_sick", 140, (1140, 150), (60, 80), flip = True)
     pygame.display.update()
     clock.tick(60)
 
