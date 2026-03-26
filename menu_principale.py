ef3ded65bdb1f09a91cbe3cd18392d489e494b35
CarraraAlessandro <carrara.alessandro.09@itisdalmine.edu.it>
2026-03-26 17:36:52 +0100

    Sitemato codice menu principale

--- menu_principale.py
+++ menu_principale.py
@@ -2,38 +2,31 @@ import pygame
 import json
 
 IMPOSTAZIONI = "./dati/setting.json"
-SFONDO = "./assets/sfondi/menu.jpg"
 
-def CaricaSettings (percorso: str):
+def CaricaSettings (percorso):
     file = open (percorso, "r", encoding="utf-8")
     info = file.read()
     dati = json.loads (info)
-    return dati["height"], dati["widht"], dati["audio"]
+    return dati["height"], dati["widht"], dati["audio"], dati["mod"]
 
-WIDHT, HEIGHT, VOLUME = CaricaSettings (IMPOSTAZIONI)
+WIDHT, HEIGHT, VOLUME, MOD = CaricaSettings (IMPOSTAZIONI)
 
 pygame.init()
 
-bg = pygame.image.load (SFONDO)
+bg = pygame.image.load ("./assets/sfondi/menu.jpg")
 bg = pygame.transform.scale (bg, (HEIGHT, WIDHT))
 
 screen = pygame.display.set_mode ((HEIGHT,WIDHT))
 pygame.display.set_caption ("Pirates of the see")
 clock = pygame.time.Clock()
 
-BUTTONS_NAME = ["play", "settings", "exit"]
+BUTTONS = {"play": [pygame.transform.scale(pygame.image.load ("./assets/tasti/play.jpg"), (180 * MOD, 90 * MOD)), WIDHT, HEIGHT/10], 
+            "settings": [pygame.transform.scale(pygame.image.load ("./assets/tasti/settings.jpg"), (180 * MOD, 90 * MOD)), WIDHT, HEIGHT/5], 
+            "exit": [pygame.transform.scale(pygame.image.load ("./assets/tasti/exit.jpg"), (180 * MOD, 90 * MOD)), WIDHT, HEIGHT/3]}
 
-BUTTONS_IMAGE = {"play": pygame.transform.scale(pygame.image.load ("./assets/tasti/play.jpg"), (70, 70)), 
-                 "settings": pygame.transform.scale(pygame.image.load ("./assets/tasti/settings.jpg"), (70, 70)), 
-                 "exit": pygame.transform.scale(pygame.image.load ("./assets/tasti/exit.jpg"), (70,70))}
-
-BUTTONS_RECT = {"play": pygame.Rect (WIDHT - BUTTONS_IMAGE["play"].get_width() * 1.5, HEIGHT/10, BUTTONS_IMAGE["play"].get_height(), BUTTONS_IMAGE["play"].get_width()), 
-                "settings": pygame.Rect (HEIGHT/3, WIDHT/3, BUTTONS_IMAGE["settings"].get_height(), BUTTONS_IMAGE["settings"].get_width()), 
-                "exit": pygame.Rect (HEIGHT/4, WIDHT/4, BUTTONS_IMAGE["exit"].get_height(), BUTTONS_IMAGE["exit"].get_width())}
-
-def DrawButtons(name: list, img: dict, card: dict, screen: pygame):
-    for b in name:
-        screen.blit (img [b], (card[b].x, card[b].y))
+def DrawButtons(pulsanti, screen):
+    for b in pulsanti:
+        screen.blit (pulsanti[b][0], (pulsanti[b][1], pulsanti[b][2]))
 
 
 menu_on = True
@@ -43,7 +36,7 @@ while menu_on:
             menu_on = False
 
     screen.blit (bg, (0,0))
-    DrawButtons (BUTTONS_NAME, BUTTONS_IMAGE, BUTTONS_RECT, screen)
+    DrawButtons (BUTTONS, screen)
     pygame.display.update()
     clock.tick(60)
 
